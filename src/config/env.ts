1b9ca22406e35a76897735f28314e4531614f77f
pvchiu <chieupham1011@gmail.com>
2026-07-20 22:57:31 +0700

    update evn

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  // API_BASE_URL: process.env.NEXT_PUBLIC_API_URL || "http://13.211.170.13:5000",
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://localhost:57466",
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_URL || "http://13.211.170.13:5000",
+//   API_BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://localhost:49392",
 };
